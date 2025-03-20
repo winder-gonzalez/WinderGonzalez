8640027d0b25f9e627fa3647518e524ca62317a1
winder <winder.gonzalez@uniminuto.edu.co>
2025-03-19 20:17:08 -0500

    ejercicioPOO

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-      System.out.println("Este es un repositorio para subir trabajos ,y estan en las ramas");
+        mascota mascota1 = new mascota();
+        mascota1.setColor("Cafe");
+        mascota1.setNombre("Princesa");
+        mascota1.setRaza("Golden Retriever");
+        mascota1.setTipo("Perra");
+
+        mascota mascota2 = new mascota();
+        mascota2.setColor("Blanco");
+        mascota2.setNombre("Mimi");
+        mascota2.setRaza("Maine Coon");
+        mascota2.setTipo("Gata");
+
+        mascota mascota3 = new mascota();
+        mascota3.setColor("Amarillo");
+        mascota3.setNombre("Spike");
+        mascota3.setRaza("Sirio");
+        mascota3.setTipo("Hamster");
+        System.out.println("-----------------------------------------------");
+        System.out.println(mascota1.getTipo()+" "+mascota1.getNombre());
+        System.out.println("Raza : "+mascota1.getRaza());
+        System.out.println("Color : "+mascota1.getColor());
+        System.out.println("-----------------------------------------------");
+        System.out.println(mascota2.getTipo()+" "+mascota2.getNombre());
+        System.out.println("Raza : "+mascota2.getRaza());
+        System.out.println("Color : "+mascota2.getColor());
+        System.out.println("-----------------------------------------------");
+        System.out.println(mascota3.getTipo()+" "+mascota3.getNombre());
+        System.out.println("Raza : "+mascota3.getRaza());
+        System.out.println("Color : "+mascota3.getColor());
+
 
     }
+
+
 }
---
+++ src/mascota.java
@@ -0,0 +1,38 @@
+public class mascota {
+    private String color;
+    private String nombre;
+    private String raza;
+    private String tipo;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
